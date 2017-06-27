457d3445216e1a838795df4788df53c68785e2b6
Saroj Kaashyap <sarojkaashyap@Sarojs-MacBook-Pro.local>
2017-06-28 04:58:52 +0530

    adding some basic web socket stuff, i think this should be sufficient to start our work

---
+++ app.js
@@ -0,0 +1,22 @@
+var server = require('websocket').server;
+let http = require('http');
+
+var socket = new server({
+    httpServer: http.createServer().listen(1337)
+});
+
+socket.on('request', function(request) {
+    var connection = request.accept(null, request.origin);
+
+    connection.on('message', function(message) {
+        console.log(message.utf8Data);
+        connection.sendUTF('hello');
+        setTimeout(function() {
+            connection.sendUTF('this is a websocket example');
+        }, 1000);
+    });
+
+    connection.on('close', function(connection) {
+        console.log('connection closed');
+    });
+});
